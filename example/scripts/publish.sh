2fbae9ec78d7bac027d0154862f7d3f53db39712
Nathan Orick <cnathanorick@gmail.com>
2020-12-15 16:04:13 -0500

    Put google analytics key in settings

--- example/scripts/publish.sh
+++ example/scripts/publish.sh
@@ -13,7 +13,7 @@ cloudFrontId="EJNN9SNOWGNCB"
 s3Path="${url}/${wedding}"
 cloundFrontPath="/weddings/${wedding}/*"
 
-bundle exec jekyll build    
+JEKYLL_ENV=production bundle exec jekyll build    
 aws s3 sync _site $s3Path $flags
 
 aws cloudfront create-invalidation --distribution-id $cloudFrontId --paths $cloundFrontPath
