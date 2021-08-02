cc928342f4b098d453e50a550a7892da74a1f36f
Nathan Orick <cnathanorick@gmail.com>
2021-08-01 22:46:32 -0400

    Remove jquery as dependency

--- _webpack/main.js
+++ _webpack/main.js
@@ -1,8 +1,6 @@
 import './main.scss';
 import { Modal } from 'bootstrap';
 import { createAddToCalendarButton } from './js/calendar';
-window.jQuery = window.$ = require('jquery');
-require('jquery.easing');
 require('./js/rsvp');
 require('./js/nav');
 window.createAddToCalendarButton = createAddToCalendarButton;
